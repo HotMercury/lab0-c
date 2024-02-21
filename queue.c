1a91527665f845556706f360e60302cb3e2a8a45
Jay <merqqcury@gmail.com>
2024-02-21 23:22:03 +0800

    Implement q_insert_head and q_insert_tail
    using list_add to add node to head
    using list_add_tail to add node to tail

--- queue.c
+++ queue.c
@@ -34,12 +34,47 @@ void q_free(struct list_head *l)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head) {
+        return false;
+    }
+    int len = strlen(s) + 1;
+    element_t *node = (element_t *) malloc(sizeof(element_t));
+    if (!node)
+        return false;
+
+    node->value = malloc(len);
+    if (!node->value) {
+        free(node);
+        return false;
+    }
+    strncpy(node->value, s, len);
+    if (list_empty(head)) {
+        list_add_tail(&node->list, head);
+    } else {
+        list_add(&node->list, head);
+    }
+
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head) {
+        return false;
+    }
+    int len = strlen(s) + 1;
+    element_t *node = (element_t *) malloc(sizeof(element_t));
+    if (!node)
+        return false;
+    node->value = malloc(len);
+    if (!node->value) {
+        free(node);
+        return false;
+    }
+    strncpy(node->value, s, len);
+    list_add_tail(&node->list, head);
+
     return true;
 }
 
