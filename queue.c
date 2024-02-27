656d055d2861784410b66f5fdcf0eb37b61a1f52
Jay <merqqcury@gmail.com>
2024-02-27 19:13:33 +0800

    Implement q_delete_mid
    using fast and slow pointer to find middle point

--- queue.c
+++ queue.c
@@ -129,6 +129,22 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head)) {
+        return false;
+    }
+    struct list_head *slow, *fast;
+    slow = fast = head->next;
+    while (fast->next != head && fast->next->next != head) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if (fast->next != head) {
+        slow = slow->next;
+    }
+    list_del(slow);
+    element_t *entry = container_of(slow, element_t, list);
+    free(entry->value);
+    free(entry);
     return true;
 }
 
